b84aa0b28aff46c42cdf9ef5045dc1d698b7b4f2
alfred-peter <alfredkilonzo90@gmail.com>
2024-06-05 11:23:01 +0300

    completed labs

--- lib/person.py
+++ lib/person.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python3
 
 class Person:
-    pass
+    def __init__(self, name):
+        self.name = name
+        print(f"Person's name is: {self.name}")
+person = Person("John")
+
